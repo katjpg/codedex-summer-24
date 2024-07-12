1a0b52b07a9652c4e35b614932e966e482770ac5
katjpg1 <kat.limqueco@bluelabs.com>
2024-07-12 04:09:32 -0700

    added share button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import Envelope from '../components/Envelope';
+import ShareButton from '../components/ShareButton';
 
+const GoogleMapComponent = dynamic(() => import('../components/GoogleMapComponent'), { 
+  ssr: false 
+});
 
-const GoogleMapComponent = dynamic(() => import('../components/GoogleMapComponent'), { ssr: false });
-
-const DraggableItem = dynamic(() => import('../components/DraggableItem'), { ssr: false });
-
-const mapContainerStyle = {
-  width: '100%',
-  height: '100%',
-};
-
-const center = {
-  lat: 40.6828, // Approximate coordinates for 2nd Pl & Court St, Brooklyn
-  lng: -73.9957,
-};
+const DraggableItem = dynamic(() => import('../components/DraggableItem'), { 
+  ssr: false 
+});
 
 export default function Home() {
   return (
-    <>
-      <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 bg-white text-black p-2">
-      </a>
-      <main id="main-content" className="flex flex-col w-full">
-        <section className="h-screen w-full flex items-center justify-center bg-[#d8d5d5]">
-          <Envelope>
+    <main className="flex flex-col w-full">
+      <section className="h-screen w-full flex items-center justify-center bg-[#d8d5d5]">
+        <Envelope>
           <div className="relative" style={{ width: '70%', height: '70%', marginLeft: '15%' }}>
             <Image
               src="/invitation.png"
@@ -34,60 +25,59 @@ export default function Home() {
               objectFit="contain"
             />
           </div>
-          </Envelope>
-        </section>
+        </Envelope>
+      </section>
 
-        <section className="h-screen w-full bg-[#d8d5d5] relative overflow-hidden">
-  <div className="w-full h-24 bg-[#BB5C68] absolute top-0 left-0 right-0 z-10 flex items-center justify-center">
-    <Image
-      src="/DETAILS.png"
-      alt="Details"
-      width={300}
-      height={300}
-      objectFit="contain"
-      className="max-h-full"
-    />
-  </div>
-  <div className="absolute inset-0 pt-24">
-    <div className="relative w-full h-full p-4 sm:p-8 md:p-16 lg:p-24 overflow-hidden">
-      <DraggableItem 
-        initialPosition={{ x: 20, y: 100 }} 
-        polaroidSrc="/polaroid-1.png"
-        contentSrc="/content-1.png"
-        size={350}
-      />
-      <DraggableItem 
-        initialPosition={{ x: 300, y: 150 }} 
-        polaroidSrc="/polaroid-2.png"
-        contentSrc="/content-2.png"
-        size={350}
-      />
-      <DraggableItem 
-        initialPosition={{ x: 580, y: 200 }} 
-        polaroidSrc="/polaroid-3.png"
-        contentSrc="/content-3.png"
-        size={350}
-      />
-      <DraggableItem
-        initialPosition={{ x: 450, y: 50 }}
-        imageSrc="/star.png"
-        size={100}
-      />
-      <DraggableItem
-        initialPosition={{ x: 100, y: 30 }}
-        imageSrc="/groovy.png"
-        size={200}
-      />
-      <DraggableItem
-        initialPosition={{ x: 600, y: 10 }}
-        imageSrc="/logo.png"
-        size={200}
-      />
-    </div>
-  </div>
-</section>
-        <section className="h-screen w-full bg-[#BB5C68] relative p-2 sm:p-4 md:p-6 lg:p-8">
-          <div className="relative w-full h-full flex">
+      <section className="h-screen w-full bg-[#d8d5d5] relative overflow-hidden">
+        <div className="w-full h-24 bg-[#BB5C68] absolute top-0 left-0 right-0 z-10 flex items-center justify-center">
+          <Image
+            src="/DETAILS.png"
+            alt="Details"
+            width={300}
+            height={300}
+            objectFit="contain"
+            className="max-h-full"
+          />
+        </div>
+        <div className="absolute inset-0 pt-16 justify-center">
+          <div className="relative w-full h-full p-10">
+            <DraggableItem 
+              initialPosition={{ x: 20, y: 100 }} 
+              imageSrc="/polaroid-1.png"
+              size={400}
+            />
+            <DraggableItem 
+              initialPosition={{ x: 510, y: 150 }} 
+              imageSrc="/polaroid-2.png"
+              size={400}
+            />
+            <DraggableItem 
+              initialPosition={{ x: 500, y: 50 }} 
+              imageSrc="/polaroid-3.png"
+              size={400}
+            />
+            <DraggableItem
+              initialPosition={{ x: 450, y: 50 }}
+              imageSrc="/star.png"
+              size={150}
+            />
+            <DraggableItem
+              initialPosition={{ x: 100, y: 30 }}
+              imageSrc="/groovy.png"
+              size={250}
+            />
+            <DraggableItem
+              initialPosition={{ x: 600, y: 10 }}
+              imageSrc="/logo.png"
+              size={250}
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="h-screen w-full bg-[#BB5C68] relative p-2 sm:p-4 md:p-6 lg:p-8">
+        <div className="relative w-full h-full flex flex-col">
+          <div className="flex-grow flex">
             <div className="w-2/3 flex">
               <div className="w-1/2 relative">
                 <div className="absolute top-0 left-0">
@@ -99,16 +89,24 @@ export default function Home() {
                     objectFit="contain"
                   />
                 </div>
-                <div className="text-center relative text-[#FFDBA6] left-[45%] top-[30%]">
-            <p className="font-dm-mono text-[20px] mb-4">ONE DAY ONLY!</p>
-            <h1 className="font-shrikhand text-[48px] leading-tight mb-2">Carroll Gardens</h1>
-            <p className="font-shrikhand text-[48px] leading-tight mb-2">Aug 7th</p>
-            <p className="font-shrikhand text-[48px] leading-tight mb-4">10am - 6pm</p>
-            <p className="font-dm-mono text-[20px] mt-4">
-              DISCOVER HIDDEN TREASURES<br />
-              AT OUR STOOP SALE SPECTACULAR!
-            </p>
-          </div>
+                <div className="text-center relative text-[#FFDBA6] left-[45%] top-[25%]">
+                  <p className="font-dm-mono text-[20px] mb-4">ONE DAY ONLY!</p>
+                  <h1 className="font-shrikhand text-[48px] leading-tight mb-2">
+                    Carroll Gardens
+                  </h1>
+                  <p className="font-shrikhand text-[48px] leading-tight mb-2">Aug 7th</p>
+                  <p className="font-shrikhand text-[48px] leading-tight mb-4">10am - 6pm</p>
+                  <p className="font-dm-mono text-[20px] mt-4">
+                    DISCOVER HIDDEN TREASURES<br />
+                    AT OUR STOOP SALE SPECTACULAR!
+                  </p>
+                  <div className="mt-4 flex justify-center">
+                    <ShareButton 
+                      title="Stoop Sale Spectacular" 
+                      text="Join us for a one-day stoop sale in Carroll Gardens!"
+                    />
+                  </div>
+                </div>
               </div>
               <div className="w-2/3 relative left-[15%]">
                 <Image
@@ -127,8 +125,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </section>
-      </main>
-    </>
+        </div>
+      </section>
+    </main>
   );
 }
